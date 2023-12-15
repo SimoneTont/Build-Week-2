31f021b8a5a1df2ff446655e347b8492ac8b6dbb
Simone Tonti <simone.tonti98@gmail.com>
2023-12-15 15:12:08 +0100

    style green

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log(window)
+
+/*const progressBar = document.querySelector('.player-wrapper input');
+
+        progressBar.addEventListener('input', function() {
+            // Calcola la percentuale di avanzamento
+            const percentage = (progressBar.value - progressBar.min) / (progressBar.max - progressBar.min) * 100;
+
+            // Aggiorna il colore solo della parte sinistra del cursore
+            progressBar.style.background = 'linear-gradient(to right, rgba(33, 215, 96, 0.84) ${percentage}%, rgba(167, 167, 167, 0.65) ${percentage}%)';
+        });
+*/
