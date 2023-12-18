06729dc8b172b7cbc6d6c707cd649b91ccdc57df
Simone Tonti <simone.tonti98@gmail.com>
2023-12-18 12:46:11 +0100

    scriptHome

--- js/scriptHomePage.js
+++ js/scriptHomePage.js
@@ -10,3 +10,47 @@ console.log(window)
             progressBar.style.background = 'linear-gradient(to right, rgba(33, 215, 96, 0.84) ${percentage}%, rgba(167, 167, 167, 0.65) ${percentage}%)';
         });
 */
+let ID="75621062"
+let URL = "https://striveschool-api.herokuapp.com/api/deezer/album/"+ID;
+
+GetAllData()
+
+let allData=[]
+
+function GetAllData ()
+{
+    fetch(URL).then(response => response.json()).then(json => {CreateCard(json)})
+}
+function CreateCard (obj)
+{
+
+    if(obj.error==undefined)
+    {
+        allData.push(obj)
+    }
+    console.log(allData)
+    console.log(allData[0].error)
+}
+
+/*const apiUrl = "https://striveschool-api.herokuapp.com/api/deezer/album";
+let allData = [];
+
+function getAllData(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      allData = allData.concat(data.items);
+
+      if (data.nextPage) {
+        return getAllData(data.nextPage);
+      } else {
+        console.log('Tutti i dati sono stati ottenuti:', allData);
+        return allData;
+      }
+    })
+    .catch(error => {
+      console.error('Errore durante la richiesta API:', error);
+    });
+}
+
+getAllData(apiUrl);*/
