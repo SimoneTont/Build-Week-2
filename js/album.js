9ca69e6e73cad0599342ab27ab2230915a1bf174
Victor <victor246257@gmail.com>
2023-12-19 14:23:45 +0100

    modifica album

--- js/album.js
+++ js/album.js
@@ -3,16 +3,22 @@ async function func() {
     .then(response=>response.json()).then(s=>{
         console.log(s.tracks.data)
         let div=document.querySelector("div .fs-9")
-        console.log(div)
+        //console.log(div)
         let p=document.querySelector(".album")
-        console.log(p)
+        //console.log(p)
         p.innerHTML=`${s.tracks.data[0].album.title}`
         let image = document.querySelector(".imageAlbum")
-        console.log(image)
+        //console.log(image)
         image.src= `${s.tracks.data[0].album.cover_medium}`
+        let image2 = document.querySelector("p img")
+        console.log(image2)
+         image2.src= `${s.tracks.data[0].album.cover_medium}`
+        let span=document.querySelector("span")
+        console.log(span)
+        span.innerHTML=` ${s.tracks.data[0].artist.name} - num. Brani ${s.tracks.data.length-1} - durata ${s.tracks.data[0].duration} min`
         s.tracks.data.forEach((e,index) => {
             let div2=document.createElement("div")
-            div2.innerHTML=`<div class="row d-flex align-items-center text-white-50 fs-9 mt-3">
+            div2.innerHTML=`<div class="row d-flex align-items-center text-white-50 fs-9 mt-3 cursore">
                                 <div class="col-1 fs-5">
                                 <p>${index+1}</p>
                                 </div>
