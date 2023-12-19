cd96d058c6e5314f8fa99ce8c8e59da3f3dea2b4
Damiano Dell'Amore <damianodellamore96@gmail.com>
2023-12-19 09:40:42 +0100

    bugfixed js search

--- js/search.js
+++ js/search.js
@@ -2,6 +2,8 @@
 
 let inputElement = document.querySelector('.prova');
 
+
+
 inputElement.addEventListener('change', function() {
     // Ottieni il valore dell'input e salvalo in una variabile
     let query = inputElement.value;
@@ -21,10 +23,10 @@ async function faiIlFetch(lol) {
             }
         });
         let data = await response.json();
-        console.log(data); // Per il debugging
+        console.log(data); // For debugging
 
         if (data && data.data && data.data.length > 0) {
-            creaCard(data.data[1]); // Passa solo il primo artista a creaCard
+            creaCard(data.data); // Pass the entire array to creaCard
         } else {
             console.log("Nessun risultato trovato");
         }
@@ -34,55 +36,13 @@ async function faiIlFetch(lol) {
 }
 
 
+
 function rimuovi(){
     let divDaSvuotare = document.querySelectorAll('.ricercheRecenti')
     divDaSvuotare.forEach(elemento => {
         elemento.innerHTML = "";
     })
 }
-
-
-
-
-// inputElement.addEventListener('change', function() {
-//     // Ottieni il valore dell'input e salvalo in una variabile
-//     let query = inputElement.value;
-// rimuovi();
-// faiIlFetch(query);
-
-
-//     // Puoi utilizzare la variabile 'query' per le tue necessità
-//    // console.log(query); // Ad esempio, stampa il valore nella console
-// });
-// async function faiIlFetch(lol) {
-//     try {
-//         let response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${lol}`, {
-//             method: 'GET',
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         });
-//         let data = await response.json();
-//         console.log(data); // For debugging
-
-//         if (data && data.data && data.data.length > 0) {
-//             creaCard(data.data); // Pass the entire array to creaCard
-//         } else {
-//             console.log("Nessun risultato trovato");
-//         }
-//     } catch (error) {
-//         console.log("ERRORE", error);
-//     }
-// }
-
-
-
-// function rimuovi(){
-//     let divDaSvuotare = document.querySelectorAll('.ricercheRecenti')
-//     divDaSvuotare.forEach(elemento => {
-//         elemento.innerHTML = "";
-//     })
-// }
 function creaCard(dati) {
     let html = `
         <div class="row align-items-start">
